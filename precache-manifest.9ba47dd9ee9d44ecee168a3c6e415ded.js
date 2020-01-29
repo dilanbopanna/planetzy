8c06d475a0d99c2a84c42555d8abc23d61078333
dilanbopanna <bopannamj@gmail.com>
2020-01-29 17:15:01 +0530

    Updates

---
+++ precache-manifest.9ba47dd9ee9d44ecee168a3c6e415ded.js
@@ -0,0 +1,258 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d30a0ab2ad65634243ec9f310184747",
+    "url": "/planetzy/index.html"
+  },
+  {
+    "revision": "90604a5e411310a36485",
+    "url": "/planetzy/static/css/2.49b833e4.chunk.css"
+  },
+  {
+    "revision": "6a3171e3d4ae669fe1ff",
+    "url": "/planetzy/static/css/main.cc1ab334.chunk.css"
+  },
+  {
+    "revision": "90604a5e411310a36485",
+    "url": "/planetzy/static/js/2.d0f7680f.chunk.js"
+  },
+  {
+    "revision": "54f6e2d0ef339619d0a432be7ac0ab75",
+    "url": "/planetzy/static/js/2.d0f7680f.chunk.js.LICENSE"
+  },
+  {
+    "revision": "6a3171e3d4ae669fe1ff",
+    "url": "/planetzy/static/js/main.238344cf.chunk.js"
+  },
+  {
+    "revision": "0e6c49838290124ad17d",
+    "url": "/planetzy/static/js/runtime-main.3ec3731f.js"
+  },
+  {
+    "revision": "39b2c3031be6b4ea96e2e3e95d307814",
+    "url": "/planetzy/static/media/Roboto-Bold.39b2c303.woff2"
+  },
+  {
+    "revision": "dc81817def276b4f21395f7ea5e88dcd",
+    "url": "/planetzy/static/media/Roboto-Bold.dc81817d.woff"
+  },
+  {
+    "revision": "e31fcf1885e371e19f5786c2bdfeae1b",
+    "url": "/planetzy/static/media/Roboto-Bold.e31fcf18.ttf"
+  },
+  {
+    "revision": "ecdd509cadbf1ea78b8d2e31ec52328c",
+    "url": "/planetzy/static/media/Roboto-Bold.ecdd509c.eot"
+  },
+  {
+    "revision": "3b813c2ae0d04909a33a18d792912ee7",
+    "url": "/planetzy/static/media/Roboto-Light.3b813c2a.woff"
+  },
+  {
+    "revision": "46e48ce0628835f68a7369d0254e4283",
+    "url": "/planetzy/static/media/Roboto-Light.46e48ce0.ttf"
+  },
+  {
+    "revision": "69f8a0617ac472f78e45841323a3df9e",
+    "url": "/planetzy/static/media/Roboto-Light.69f8a061.woff2"
+  },
+  {
+    "revision": "a990f611f2305dc12965f186c2ef2690",
+    "url": "/planetzy/static/media/Roboto-Light.a990f611.eot"
+  },
+  {
+    "revision": "4d9f3f9e5195e7b074bb63ba4ce42208",
+    "url": "/planetzy/static/media/Roboto-Medium.4d9f3f9e.eot"
+  },
+  {
+    "revision": "574fd0b50367f886d359e8264938fc37",
+    "url": "/planetzy/static/media/Roboto-Medium.574fd0b5.woff2"
+  },
+  {
+    "revision": "894a2ede85a483bf9bedefd4db45cdb9",
+    "url": "/planetzy/static/media/Roboto-Medium.894a2ede.ttf"
+  },
+  {
+    "revision": "fc78759e93a6cac50458610e3d9d63a0",
+    "url": "/planetzy/static/media/Roboto-Medium.fc78759e.woff"
+  },
+  {
+    "revision": "2751ee43015f9884c3642f103b7f70c9",
+    "url": "/planetzy/static/media/Roboto-Regular.2751ee43.woff2"
+  },
+  {
+    "revision": "30799efa5bf74129468ad4e257551dc3",
+    "url": "/planetzy/static/media/Roboto-Regular.30799efa.eot"
+  },
+  {
+    "revision": "ba3dcd8903e3d0af5de7792777f8ae0d",
+    "url": "/planetzy/static/media/Roboto-Regular.ba3dcd89.woff"
+  },
+  {
+    "revision": "df7b648ce5356ea1ebce435b3459fd60",
+    "url": "/planetzy/static/media/Roboto-Regular.df7b648c.ttf"
+  },
+  {
+    "revision": "7500519de3d82e33d1587f8042e2afcb",
+    "url": "/planetzy/static/media/Roboto-Thin.7500519d.woff"
+  },
+  {
+    "revision": "94998475f6aea65f558494802416c1cf",
+    "url": "/planetzy/static/media/Roboto-Thin.94998475.ttf"
+  },
+  {
+    "revision": "954bbdeb86483e4ffea00c4591530ece",
+    "url": "/planetzy/static/media/Roboto-Thin.954bbdeb.woff2"
+  },
+  {
+    "revision": "dfe56a876d0282555d1e2458e278060f",
+    "url": "/planetzy/static/media/Roboto-Thin.dfe56a87.eot"
+  },
+  {
+    "revision": "11911410dca2de148f30954eb2fd5eab",
+    "url": "/planetzy/static/media/arrow_left.11911410.svg"
+  },
+  {
+    "revision": "8ef6a08cdc1154920165680a4edde771",
+    "url": "/planetzy/static/media/arrow_right.8ef6a08c.svg"
+  },
+  {
+    "revision": "ed404fb4ae6e4f784258ac9f5d76d49a",
+    "url": "/planetzy/static/media/earth-transparent.ed404fb4.png"
+  },
+  {
+    "revision": "b455babf9176eb548354897bd4a9c8a7",
+    "url": "/planetzy/static/media/earth.b455babf.png"
+  },
+  {
+    "revision": "088a34f78f530102fd9661173b4a4f26",
+    "url": "/planetzy/static/media/fa-brands-400.088a34f7.eot"
+  },
+  {
+    "revision": "273dc9bf9778fd37fa61357645d46a28",
+    "url": "/planetzy/static/media/fa-brands-400.273dc9bf.ttf"
+  },
+  {
+    "revision": "822d94f19fe57477865209e1242a3c63",
+    "url": "/planetzy/static/media/fa-brands-400.822d94f1.woff2"
+  },
+  {
+    "revision": "d72293118cda50ec50c39957d9d836d0",
+    "url": "/planetzy/static/media/fa-brands-400.d7229311.svg"
+  },
+  {
+    "revision": "f4920c94c0861c537f72ba36590f6362",
+    "url": "/planetzy/static/media/fa-brands-400.f4920c94.woff"
+  },
+  {
+    "revision": "3ac49cb33f43a6471f21ab3df40d1b1e",
+    "url": "/planetzy/static/media/fa-regular-400.3ac49cb3.eot"
+  },
+  {
+    "revision": "9efb86976bd53e159166c12365f61e25",
+    "url": "/planetzy/static/media/fa-regular-400.9efb8697.woff2"
+  },
+  {
+    "revision": "a57bcf76c178aee452db7a57b75509b6",
+    "url": "/planetzy/static/media/fa-regular-400.a57bcf76.woff"
+  },
+  {
+    "revision": "d2e53334c22a9a4937bc26e84b36e1e0",
+    "url": "/planetzy/static/media/fa-regular-400.d2e53334.svg"
+  },
+  {
+    "revision": "ece54318791c51b52dfdc689efdb6271",
+    "url": "/planetzy/static/media/fa-regular-400.ece54318.ttf"
+  },
+  {
+    "revision": "2aa6edf8f296a43b32df35f330b7c81c",
+    "url": "/planetzy/static/media/fa-solid-900.2aa6edf8.ttf"
+  },
+  {
+    "revision": "7a5de9b08012e4da40504f2cf126a351",
+    "url": "/planetzy/static/media/fa-solid-900.7a5de9b0.svg"
+  },
+  {
+    "revision": "7fb1cdd9c3b889161216a13267b55fe2",
+    "url": "/planetzy/static/media/fa-solid-900.7fb1cdd9.eot"
+  },
+  {
+    "revision": "93f284548b42ab76fe3fd03a9d3a2180",
+    "url": "/planetzy/static/media/fa-solid-900.93f28454.woff"
+  },
+  {
+    "revision": "f6121be597a72928f54e7ab5b95512a1",
+    "url": "/planetzy/static/media/fa-solid-900.f6121be5.woff2"
+  },
+  {
+    "revision": "3d1af82f27bc71f016f0cb58c687063f",
+    "url": "/planetzy/static/media/jupiter-transparent.3d1af82f.png"
+  },
+  {
+    "revision": "eebc33da3375d1217f523b528c607d6e",
+    "url": "/planetzy/static/media/jupiter.eebc33da.png"
+  },
+  {
+    "revision": "93fe18634bf9e3ace0488730946ca294",
+    "url": "/planetzy/static/media/logo.93fe1863.png"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/planetzy/static/media/logo.ee7cd8ed.svg"
+  },
+  {
+    "revision": "055cb983a261321d22e1274819b7906c",
+    "url": "/planetzy/static/media/mars-transparent.055cb983.png"
+  },
+  {
+    "revision": "eb13284c6208bf65471e1b9b8f0db440",
+    "url": "/planetzy/static/media/mars.eb13284c.png"
+  },
+  {
+    "revision": "bdc4441e309c201542c1d223dd85baf6",
+    "url": "/planetzy/static/media/mercury-transparent.bdc4441e.png"
+  },
+  {
+    "revision": "6cf7fee771fad57b5dbe3a9281b56f06",
+    "url": "/planetzy/static/media/mercury.6cf7fee7.png"
+  },
+  {
+    "revision": "af3f44046c0159ac8dee297aaa39fbad",
+    "url": "/planetzy/static/media/neptune-transparent.af3f4404.png"
+  },
+  {
+    "revision": "8ea18d47f8b6fe765d85d5a9801deea9",
+    "url": "/planetzy/static/media/neptune.8ea18d47.png"
+  },
+  {
+    "revision": "a20fe87acdd25a1431ab3d8a86bdb59c",
+    "url": "/planetzy/static/media/pluto.a20fe87a.png"
+  },
+  {
+    "revision": "9672268f036f88b16cc9d0a32bd24946",
+    "url": "/planetzy/static/media/saturn-transparent.9672268f.png"
+  },
+  {
+    "revision": "51043e94ac0dbbf689472a720cc33db3",
+    "url": "/planetzy/static/media/saturn.51043e94.png"
+  },
+  {
+    "revision": "0e464b1c6cd77ff4c3d1249dd9cd0f70",
+    "url": "/planetzy/static/media/sun.0e464b1c.png"
+  },
+  {
+    "revision": "4bb449ca254cce3d40bf9d148ed41b77",
+    "url": "/planetzy/static/media/uranus-transparent.4bb449ca.png"
+  },
+  {
+    "revision": "713fbc0000dd8f3bbd7effef96bb6422",
+    "url": "/planetzy/static/media/uranus.713fbc00.png"
+  },
+  {
+    "revision": "4824b1bb8fecc2825b49c4a70c6c43e0",
+    "url": "/planetzy/static/media/venus-transparent.4824b1bb.png"
+  },
+  {
+    "revision": "6414047e9b508e99633e573af326bfd6",
+    "url": "/planetzy/static/media/venus.6414047e.png"
+  }
+]);
